cfb71b9c441ed106b124a348a61851984adb824f
Benjamin Davies <bentendavies@gmail.com>
2022-05-26 10:44:04 +1200

    Add token parsing tests

--- src/tokens.rs
+++ src/tokens.rs
@@ -45,3 +45,33 @@ pub fn parse_number<I: FromStr<Err = ParseIntError>>(mut raw: &[u8]) -> ParseRes
 
   Ok((number, &raw[length..]))
 }
+
+#[cfg(test)]
+mod test {
+  use super::*;
+
+  #[test]
+  fn should_peek_char() {
+    assert_eq!(peek_char(b"Hello, world!").unwrap(), 'H');
+  }
+
+  #[test]
+  fn should_parse_whitespace() {
+    let ((), rest) = parse_whitespace(b" \t \r\nHello, world!").unwrap();
+    assert_eq!(rest, b"Hello, world!");
+  }
+
+  #[test]
+  fn should_parse_keyword() {
+    let (keyword, rest) = parse_keyword(b"  keyword  ").unwrap();
+    assert_eq!(keyword, "keyword");
+    assert_eq!(rest, b"  ");
+  }
+
+  #[test]
+  fn should_parse_number() {
+    let (number, rest) = parse_number::<usize>(b"  42  ").unwrap();
+    assert_eq!(number, 42);
+    assert_eq!(rest, b"  ");
+  }
+}
